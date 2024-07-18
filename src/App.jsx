0c4d97880bcc5b4bf6dfff62cd24196987cec003
Dumitru <dima50066@gmail.com>
2024-07-18 16:33:25 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import Profile from './components/Profile';
 import userData from "./data/userData.json";
 
-function App () {
+function App() {
   return (
-<>
-  <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-      
-  />
-  </>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+
+      />
+    </>
   );
-  };
+};
 
 
 export default App
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,46 +1,31 @@
-import styles from './profile.module.css';
-import PropTypes from 'prop-types';
+import css from './profile.module.css';
 
-
-    
-const Profile = ({ props }) => {
+const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{username}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.profileCard}>
+      <div className={css.profileMainInfo}>
+        <img className={css.profileAvatar} src={image} alt={name} />
+        <p className={css.profileNameText}>{name}</p>
+        <p className={css.profileInfoText}>@{tag}</p>
+        <p className={css.profileInfoText}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={css.profileStatsList}>
+        <li className={css.profileStatsListItem}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css.profileStatsText}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.profileStatsListItem}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css.profileStatsText}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.profileStatsListItem}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css.profileStatsText}>{stats.likes}</span>
         </li>
       </ul>
-    </>
+    </div>
   );
 };
-
-// Profile.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string.isRequired,
-//   stats: PropTypes.shape({
-//     followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
 
 export default Profile;
