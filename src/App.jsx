e5b223095e02680bfef76d183e5d83fdfd9e2eaf
Dumitru <dima50066@gmail.com>
2024-07-18 17:18:00 +0200

    add task 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Profile from "./components/Profile/Profile";
 import userData from "./data/userData.json";
 import friends from "./data/friends.json";
 import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./data/transactions.json";
 import "./App.css";
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,28 @@
+import css from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item) => {
+          return (
+            <tr className={css.tableRow} key={item.id}>
+              <td className={css.transactionType}>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+export default TransactionHistory;
